956d49a038eae482ade4ff5f82310e9a6eabcf5a
Nemo <kanghaov@vip.qq.com>
2022-12-09 17:25:05 +0800

    修改：     eBook/exercises/chapter_10/vcard.go 新增解法 一个func方法

--- eBook/exercises/chapter_10/vcard.go
+++ eBook/exercises/chapter_10/vcard.go
@@ -68,3 +68,58 @@ func main() {
 	Nemo := &VCard{"Nemo",&Address{111},"1994-01-26","xxx"}
 	fmt.Printf("Nemo's name:%s,addressCode:%d,birthday:%s,image:%s",Nemo.name,Nemo.addressCode.addressCode,Nemo.birthday,Nemo.image)
 }
+
+//或者
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+type Address struct {
+	Street           string
+	HouseNumber      uint32
+	HouseNumberAddOn string
+	POBox            string
+	ZipCode          string
+	City             string
+	Country          string
+}
+
+type VCard struct {
+	FirstName string
+	LastName  string
+	NickName  string
+	BirtDate  time.Time
+	Photo     string
+	Address   map[string]*Address
+}
+
+func NewAddress(name string, Street string, HouseNumber uint32, HouseNumberAddOn string, POBox string, ZipCode string, City string, Country string) *Address {
+	name := Address{
+		Street: Street,
+		HouseNumber: HouseNumber,
+		HouseNumberAddOn: HouseNumberAddOn,
+		POBox: POBox,
+		ZipCode: ZipCode,
+		City:City,
+		Country: Country
+	}
+	return &name
+}
+
+
+
+func main() {
+	addr1 := NewAddress("youth","Elfenstraat", 12, "", "", "2600", "Mechelen", "België")
+	addr2 := NewAddress{"now","Heideland", 28, "", "", "2640", "Mortsel", "België"}
+	addrs := make(map[string]*Address, 2)
+	addrs["youth"] = addr1
+	addrs["now"] = addr2
+	birthDate := time.Date(1956,1,17,15,4,5,0,time.Local) 
+	photo := "MyDocuments/MyPhotos/photo1.jpg"
+	vcard := &VCard{"Ivo", "Balbaert", "", birthdt, photo, addrs}
+	fmt.Printf("Here is the full VCard: %v\n", vcard)
+	fmt.Printf("My Addresses are:\n %v\n %v", addr1, addr2)
+}
